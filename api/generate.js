7835f9bd7c6ad9cb980b2b120a80361796413986
paganilaurent95-cmd <pagani.laurent.95@gmail.com>
2026-02-16 18:01:35 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,3 +1,12 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "API fonctionne 🚀" });
+export default async function handler(req, res) {
+
+  if (req.method === "GET") {
+    return res.status(200).json({ message: "API fonctionne 🚀" });
+  }
+
+  if (req.method === "POST") {
+    return res.status(200).json({ message: "POST reçu ✅" });
+  }
+
+  return res.status(405).json({ error: "Méthode non autorisée" });
 }
