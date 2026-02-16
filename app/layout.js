fc0a3e2d8d967ef44799797e6e60f9c390377837
paganilaurent95-cmd <pagani.laurent.95@gmail.com>
2026-02-16 19:10:12 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Inspire API"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
